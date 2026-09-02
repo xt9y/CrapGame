0569ea2c1f31ec0693b35ab49982b79c0b3bffb8
xt9y <felix@xt9y.de>
2026-09-03 00:02:00 +0200

    Renderer Performance Stage 05: allow zero-readback GPU presentation

--- Sources/Renderer/Gpu/Presenter.hpp
+++ Sources/Renderer/Gpu/Presenter.hpp
@@ -18,15 +18,19 @@ public:
     bool init (std::string *error = nullptr);
     bool resize (int width, int height, std::string *error = nullptr);
     bool present (const std::vector<std::uint8_t>& rgb, std::string *error = nullptr);
+    bool presentTexture (GLuint texture, std::string *error = nullptr);
     void shutdown ();
 
     bool ready () const;
 
 private:
+    bool drawTexture (GLuint texture, bool flip_y, std::string *error);
+
     GLuint program_ = 0;
     GLuint texture_ = 0;
     GLuint vao_ = 0;
     GLint texture_location_ = -1;
+    GLint flip_y_location_ = -1;
 
     int width_ = 0;
     int height_ = 0;
