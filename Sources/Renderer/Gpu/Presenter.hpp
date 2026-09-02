99dfe9c362b3783bfcd4b15987e2dc7571b6c6c3
xt9y <felix@xt9y.de>
2026-09-02 23:45:59 +0200

    Renderer Performance Stage 03: add GPU presenter interface

---
+++ Sources/Renderer/Gpu/Presenter.hpp
@@ -0,0 +1,38 @@
+#ifndef CRAPGAME_RENDERER_GPU_PRESENTER_HPP
+#define CRAPGAME_RENDERER_GPU_PRESENTER_HPP
+
+#include <lwcgl/lwcgl.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace Renderer
+{
+namespace Gpu
+{
+
+class Presenter
+{
+public:
+    bool init (std::string *error = nullptr);
+    bool resize (int width, int height, std::string *error = nullptr);
+    bool present (const std::vector<std::uint8_t>& rgb, std::string *error = nullptr);
+    void shutdown ();
+
+    bool ready () const;
+
+private:
+    GLuint program_ = 0;
+    GLuint texture_ = 0;
+    GLuint vao_ = 0;
+    GLint texture_location_ = -1;
+
+    int width_ = 0;
+    int height_ = 0;
+};
+
+} // namespace Gpu
+} // namespace Renderer
+
+#endif
