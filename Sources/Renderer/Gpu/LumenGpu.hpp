773d0ec46ddae0d44b32a84a2e9fa505cd0945de
xt9y <felix@xt9y.de>
2026-09-03 00:39:18 +0200

    Renderer Performance Stage 13: share primitive buffer with Lumen

--- Sources/Renderer/Gpu/LumenGpu.hpp
+++ Sources/Renderer/Gpu/LumenGpu.hpp
@@ -9,10 +9,8 @@
 #include <lwcgl/lwcgl.h>
 #include <lwcgl/glmodern.h>
 
-#include <cstddef>
 #include <cstdint>
 #include <string>
-#include <vector>
 
 namespace Renderer
 {
@@ -44,21 +42,10 @@ public:
     GLuint reflectionTexture () const { return reflection_history_[history_index_]; }
 
 private:
-    struct PrimitiveGpu
-    {
-        float position_type[4];
-        float rotation[4];
-        float scale[4];
-        float albedo_metallic[4];
-        float emissive_roughness[4];
-    };
-
-    bool uploadPrimitives (const Ecs::World& world, std::string *error);
     void destroyTextures ();
 
     GLuint trace_program_ = 0;
     GLuint composite_program_ = 0;
-    GLuint primitive_buffer_ = 0;
 
     GLuint indirect_history_[2] = {0, 0};
     GLuint reflection_history_[2] = {0, 0};
@@ -70,9 +57,6 @@ private:
     GLint trace_primitive_count_location_ = -1;
     GLint trace_frame_location_ = -1;
     GLint trace_history_valid_location_ = -1;
-
-    std::size_t primitive_capacity_ = 0;
-    std::vector<PrimitiveGpu> primitives_;
 
     int history_index_ = 0;
     bool history_valid_ = false;
