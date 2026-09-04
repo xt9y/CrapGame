8a584df9ee2e0c69c89f97bd2aacea3aa738455e
xt9y <felix@xt9y.de>
2026-09-04 12:34:34 +0200

    Tests: match transparent IOR helper contract

--- tests/transparent_material_contract.cpp
+++ tests/transparent_material_contract.cpp
@@ -3,4 +3,4 @@
 #include <iterator>
 #include <string>
 static std::string read(const char*p){std::ifstream in(p);return std::string((std::istreambuf_iterator<char>(in)),{});}
-int main(){auto s=read("Sources/Renderer/Gpu/TransparentGpu.cpp");assert(s.find("RenderClass::Transparent")!=std::string::npos);assert(s.find("RenderClass::Transmissive")!=std::string::npos);assert(s.find("stable_sort")!=std::string::npos);assert(s.find("a.depth>b.depth")!=std::string::npos);assert(s.find("GL_ONE_MINUS_SRC_ALPHA")!=std::string::npos);assert(s.find("sOpaque")!=std::string::npos);assert(s.find("iorF0")!=std::string::npos);assert(s.find("GL_READ_FRAMEBUFFER")!=std::string::npos);}
+int main(){auto s=read("Sources/Renderer/Gpu/TransparentGpu.cpp");assert(s.find("RenderClass::Transparent")!=std::string::npos);assert(s.find("RenderClass::Transmissive")!=std::string::npos);assert(s.find("stable_sort")!=std::string::npos);assert(s.find("a.depth>b.depth")!=std::string::npos);assert(s.find("GL_ONE_MINUS_SRC_ALPHA")!=std::string::npos);assert(s.find("sOpaque")!=std::string::npos);assert(s.find("iorToF0")!=std::string::npos);assert(s.find("GL_READ_FRAMEBUFFER")!=std::string::npos);}
