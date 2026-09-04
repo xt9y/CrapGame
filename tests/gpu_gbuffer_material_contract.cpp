b38c574763e564ad95c3faf503f370734e07e449
xt9y <felix@xt9y.de>
2026-09-05 01:44:43 +0200

    Test seven-target GBuffer layout

--- tests/gpu_gbuffer_material_contract.cpp
+++ tests/gpu_gbuffer_material_contract.cpp
@@ -12,7 +12,9 @@ int main()
     assert(source.find("uNormalTex") != std::string::npos);
     assert(source.find("uOpacityTex") != std::string::npos);
     assert(source.find("discard") != std::string::npos);
-    assert(source.find("GL_COLOR_ATTACHMENT7") != std::string::npos);
+    assert(source.find("GL_COLOR_ATTACHMENT6") != std::string::npos);
+    assert(source.find("GL_COLOR_ATTACHMENT7") == std::string::npos);
+    assert(source.find("glDrawBuffers(7") != std::string::npos);
     assert(source.find("renderer_material") != std::string::npos);
     assert(source.find("RenderClass::Transparent") != std::string::npos);
     assert(source.find("MaterialGpu::LIVE_TEXTURE_COUNT") != std::string::npos);
