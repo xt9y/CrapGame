704169a1e4d05c78374d07650a3032b2c3417c91
xt9y <felix@xt9y.de>
2026-09-04 20:17:52 +0200

    Perf: define static shadow cache policy

---
+++ Sources/Renderer/Gpu/StaticShadowPolicy.hpp
@@ -0,0 +1,19 @@
+#ifndef CRAPGAME_RENDERER_GPU_STATICSHADOWPOLICY_HPP
+#define CRAPGAME_RENDERER_GPU_STATICSHADOWPOLICY_HPP
+
+namespace Renderer
+{
+namespace Gpu
+{
+
+struct StaticShadowPolicy
+{
+    static constexpr int SIZE = 2048;
+    static constexpr int PCF_RADIUS = 1;
+    static constexpr float PADDING = 0.05f;
+};
+
+} // namespace Gpu
+} // namespace Renderer
+
+#endif
